4c441fb09b04d15978f4fc69e67324302c4f1062
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-09-29 11:19:54 +0200

    create file

---
+++ cw_nr_3.py
@@ -0,0 +1,14 @@
+#  wszystkie elementy listy, które są mniejsze niż 5
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+
+
+'''
+Dodatki:
+
+Zamiast drukować elementy jeden po drugim, stwórz nową listę zawierającą wszystkie elementy 
+mniejsze niż 5 z tej listy i wydrukuj tę nową listę. Napisz to w jednym wierszu Pythona.
+Poproś użytkownika o numer i zwróć listę zawierającą tylko elementy z oryginalnej listy a, 
+które są mniejsze niż liczba podana przez użytkownika.
+
+'''
