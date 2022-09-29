f482f5d9259fc9395a7d7836ec3720c23bc73d84
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2022-09-30 00:54:49 +0200

    update

---
+++ cw_nr_4.py
@@ -0,0 +1,7 @@
+# Utwórz program, który pyta użytkownika o liczbę, a następnie wyświetla listę wszystkich dzielników tej liczby
+
+liczba = int(input('Podaj liczbę: '))
+
+l = []
+l.append(liczba)
+print(l)
